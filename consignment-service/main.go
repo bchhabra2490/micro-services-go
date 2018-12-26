839414d2c7135721b84fd05943207a9e50aa2d84
bchhabra2490 <bchhabra2490@gmail.com>
2018-12-26 14:19:31 +0530

    Added vessel-service

--- consignment-service/main.go
+++ consignment-service/main.go
@@ -6,41 +6,54 @@ import (
 	"fmt"
 	micro "github.com/micro/go-micro"
 	"golang.org/x/net/context"
+	"log"
 	pb "services/consignment-service/proto/consignment"
+	vesselProto "services/vessel-service/proto/vessel"
 )
 
-type IRepository interface {
+type Repository interface {
 	Create(*pb.Consignment) (*pb.Consignment, error)
 	GetAll() []*pb.Consignment
 }
 
-type Repository struct {
+type ConsignmentRepository struct {
 	consignments []*pb.Consignment
 }
 
-func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
+func (repo *ConsignmentRepository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
 	updated := append(repo.consignments, consignment)
 	repo.consignments = updated
 	return consignment, nil
 }
 
-func (repo *Repository) GetAll() []*pb.Consignment {
+func (repo *ConsignmentRepository) GetAll() []*pb.Consignment {
 	return repo.consignments
 }
 
 type service struct {
-	repo IRepository
+	repo         Repository
+	vesselClient vesselProto.VesselServiceClient
 }
 
 func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
 
-	// Save our consignment
-	consignment, err := s.repo.Create(req)
+	vesselResponse, err := s.vesselClient.FindAvailable(context.Background(), &vesselProto.Specification{
+		MaxWeight: req.Weight,
+		Capacity:  int32(len(req.Containers)),
+	})
+	log.Printf("Found vessel: %s \n", vesselResponse.Vessel.Name)
 
 	if err != nil {
 		return err
 	}
 
+	req.VesselId = vesselResponse.Vessel.Id
+
+	// Save out consignment
+	consignment, err := s.repo.Create(req)
+	if err != nil {
+		return err
+	}
 	res.Created = true
 	res.Consignment = consignment
 
@@ -54,7 +67,7 @@ func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, res *
 }
 
 func main() {
-	repo := &Repository{}
+	repo := &ConsignmentRepository{}
 
 	// Create a new service.
 	srv := micro.NewService(
@@ -67,7 +80,7 @@ func main() {
 	srv.Init()
 
 	// Register handler
-	pb.RegisterShippingServiceHandler(srv.Server(), &service{repo})
+	pb.RegisterShippingServiceHandler(srv.Server(), &service{repo, vesselClient})
 
 	if err := srv.Run(); err != nil {
 		fmt.Println(err)
